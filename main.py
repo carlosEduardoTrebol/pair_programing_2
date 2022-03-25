5dea76ddaf8bd71add7385dcf87cd950e0cd9d36
carlosEduardoTrebol <carlos.pacichana@grupotrebolenergia.es>
2022-03-25 14:20:04 +0100

    refactor: dictionary step, green test

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 
 class Roman_Numbers():
     def roman_number_transform_to_decimal(self, num):
-        if num == 1:
-            return "i"
-        elif num == 2:
-            return "ii"
-        elif num == 3:
-            return "iii"
+
+        letter_values = {1:"i",2:"ii",3:"iii"}
+        return letter_values[num]
